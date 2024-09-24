d41eae4615096c04cbf7a2008dc5ffaf3165566c
Junior36912 <juniojackson369@gmail.com>
2024-09-23 21:47:58 -0300

    Adicionando ArrowFunction

---
+++ function_arrow.js
@@ -0,0 +1,8 @@
+const exibeNomeArrow = (nome) => nome;
+
+const exibeNomeTradicional = (nome) => {
+    return nome;
+}
+
+console.log(exibeNomeArrow("Junior"))
+console.log(exibeNomeTradicional("Jackson"))
