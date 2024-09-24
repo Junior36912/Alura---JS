390f6e1bf9f48394cc5c977e79c240ef85ac627d
Junior36912 <juniojackson369@gmail.com>
2024-09-23 21:55:14 -0300

    Adicionando Maior de Idade

---
+++ maior_de_idade.js
@@ -0,0 +1,8 @@
+function verificaIdade(idade) {
+    return idade >= 18;
+}
+
+// Chamada da função
+console.log(verificaIdade(20));  // Exemplo com idade 20
+
+console.log(verificaIdade(4));  // Exemplo com idade 20
