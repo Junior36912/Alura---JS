7e17319906071e978cf787cf86af6202ac0fc151
Junior36912 <juniojackson369@gmail.com>
2024-09-23 22:09:48 -0300

    Atualizado Potencia

--- potencia.js
+++ potencia.js
@@ -1,4 +1,4 @@
 const calculaPotencia = (base, expoente) => base ** expoente;
 
-console.log(calculaPotencia(2, 3));  // Exemplo com base 2 e expoente 3
-console.log(calculaPotencia(3, 2));
+console.log("2 elevado a 3° potencia", calculaPotencia(2, 3));  // Exemplo com base 2 e expoente 3
+console.log("3 elevado a 2° potencia",calculaPotencia(3, 2));
